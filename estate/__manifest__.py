8f95756ebd1f254fc168c0803143c96c70a408c0
“harsh” <hdjethva6@gmail.com>
2022-01-11 15:41:07 +0530

    1

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,4 +2,9 @@
     "name" : "estate",	
     "category" : "sales",
     "application" : True,
+    "data" : [
+        "/home/harsh/dev/custom/estate/security/ir.model.access.csv",
+        "/home/harsh/dev/custom/estate/views/estate_manus.xml",
+        "/home/harsh/dev/custom/estate/views/estate_property_views.xml",
+        ],
 }
---
+++ odootest/estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name" : "estate",	
+    "category" : "sales",
+    "application" : True,
+    "data" : [
+        "/home/harsh/dev/custom2/estate/security/ir.model.access.csv",
+        "/home/harsh/dev/custom2/estate/views/estate_manus.xml",
+        "/home/harsh/dev/custom2/estate/views/estate_property_views.xml",
+        ],
+}
