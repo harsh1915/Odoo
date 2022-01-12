755445cbe6045ae32d8e2e5ef0af2dd7047725c8
“harsh” <hdjethva6@gmail.com>
2022-01-12 18:34:29 +0530

    property type

--- odootest/estate/__manifest__.py
+++ odootest/estate/__manifest__.py
@@ -3,8 +3,8 @@
     "category" : "sales",
     "application" : True,
     "data" : [
-        "/home/harsh/dev/custom2/estate/security/ir.model.access.csv",
-        "/home/harsh/dev/custom2/estate/views/estate_manus.xml",
-        "/home/harsh/dev/custom2/estate/views/estate_property_views.xml",
+        "/home/harsh/dev/custom/odootest/estate/security/ir.model.access.csv",
+        "/home/harsh/dev/custom/odootest/estate/views/estate_manus.xml",
+        "/home/harsh/dev/custom/odootest/estate/views/estate_property_views.xml",
         ],
 }
