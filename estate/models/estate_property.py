7a27ae8a85bbcad0220c2208ed4b7c7572470afa
“harsh” <hdjethva6@gmail.com>
2022-01-19 10:12:39 +0530

    compute

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,8 +1,10 @@
-from odoo import models,fields
+from email.policy import default
+from odoo import models,fields,api
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Estate Property Offer"
+    _rec_name = 'property_id'
 
     price = fields.Float()
     status = fields.Selection([
@@ -55,3 +57,34 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner")
     property_tag_id = fields.Many2many("estate.property.tag")
     property_offer_id = fields.One2many("estate.property.offer", "property_id")
+    total_area = fields.Integer(compute="_compute_area", inverse="_inverse_area")
+    best_price = fields.Float(compute="_compute_best_price")
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline")
+
+
+
+    @api.depends("living_area", "garden_area")
+    def _compute_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends("property_offer_id.price")
+    def _compute_best_price(self):
+        for record in self:
+            max_price=0
+            for offer in record.property_offer_id:
+                if offer.price > max_price:
+                    max_price = offer.price
+            record.best_price = max_price
+
+
+    def _inverse_area(self):
+        for record in self:
+            record.living_area = record.garden_area = record.total_area/2
+
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(record.date_availability, days=7)
